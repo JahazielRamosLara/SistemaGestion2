bd259920f9729bbc090a7af1d27077381bb85caa
Jahaziel Ramos <jachachel@gmail.com>
2025-11-16 19:15:00 -0600

    Configuración vuelve a ser en gmail

--- SistemaGestion/settings.py
+++ SistemaGestion/settings.py
@@ -140,7 +140,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
